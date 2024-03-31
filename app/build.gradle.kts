3709d79e56b29135848b666c7288b75593150b06
hvenanc <hvs.git@gmail.com>
2024-03-31 16:44:15 -0300

    Prática 03: Adição da dependencia lifecycle-viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 }
