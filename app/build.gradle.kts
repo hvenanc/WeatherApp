bfebece49deb9dea0988fa3d31b18f8c2be7f126
hvenanc <hvs.git@gmail.com>
2024-08-21 14:24:45 -0300

    [Prática 06 - Conexão com o Firestore]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
